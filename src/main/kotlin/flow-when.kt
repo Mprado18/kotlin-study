135b3d2c86fa24a09127dd16f4c00bd43448d414
Marcos-Prado <marcos.prado@baritecnologia.com.br>
2021-04-19 19:10:32 -0300

    about flow when

---
+++ src/main/kotlin/flow-when.kt
@@ -0,0 +1,34 @@
+// when é um fluxo de controle assim como o if-else, onde podemos colocar condicionais
+// segue a mesma lógica de switch e case das outras linguagens
+
+fun operations(a: Int, b: Int, c: String): Int {
+    when (c) {
+        "+" -> return a + b
+        "-" -> return a - b
+        else -> {
+            println("operação incorreta")
+            return 0
+        }
+    }
+}
+
+fun validate(a: Int) {
+    // Aqui estamos utilizando Range, que é um intervalo de valores - ..
+    when (a) {
+        in 1..99 -> {
+            println("Valor está entre 1 e 99")
+        }
+        else -> {
+            println("Valor não está entre 1 e 99")
+        }
+    }
+}
+
+fun main() {
+    println(operations(10,15,"+"))
+    println(operations(15,10,"-"))
+    println(operations(10,15,"*"))
+
+    validate(15)
+    validate(100)
+}
