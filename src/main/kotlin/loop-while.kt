38d7ce748c1c726d6246fdd65d5b74fb6650bbb5
Marcos-Prado <marcos.prado@baritecnologia.com.br>
2021-04-19 19:32:57 -0300

    about loop while

---
+++ src/main/kotlin/loop-while.kt
@@ -0,0 +1,33 @@
+// além do for temos o while e também o do while para estrutura de repetições (loop)
+// while é mais utilizado para quando não sabemos qual é o range a ser percorrido
+
+fun main() {
+
+    // devemos cuidar com loops infinitos
+    // abaixo um exemplo de loop infinito, como não temos condição de parada, seguirá rodando
+    /*
+    while (true) {
+        println("Marcos")
+    }
+     */
+
+    // termina a execução em 90 devido o while parar 1 antes
+    var index = 10
+    while (index < 100) {
+        println("index: $index")
+        index += 10
+    }
+
+    var i = 100
+    while (i > 1) {
+        println("i: $i ")
+        i -= 10
+
+        // estamos colocando uma condição de parada do loop
+        // caso resultado do if seja true o break para execução do loop
+        if (i == 50) {
+            break
+        }
+    }
+
+}
