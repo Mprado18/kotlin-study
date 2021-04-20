03add36894cad17cc986c42a7270af7d44c63438
Marcos-Prado <marcosprprado@yahoo.com.br>
2021-04-20 20:23:54 -0300

    updating content about null safe

--- src/main/kotlin/null-safe-and-elvis.kt
+++ src/main/kotlin/null-safe-and-elvis.kt
@@ -3,8 +3,8 @@
 
 fun main() {
 
-    // declarar a forma abaixo quer dizer que a variável pode ser nula
-    // se não for colocado o operador safe ?, teremos o erro de NullPointerException
+    // declarar da forma abaixo quer dizer que a variável pode ser nula
+    // se não for colocado o operador safe ?, teremos o erro de NullPointerException (erro chatão)
 
     val test: Int? = null
     val op = test ?: "novo valor string, expressão, number"
@@ -16,4 +16,16 @@ fun main() {
 
     println(y)
     println(z)
+
+    // da forma abaixo dizemos que a variável pode ser nula e assim podemos acessar as propriedades
+    // caso não digamos que a variável pode ser nula ao tentar acessar as propriedades, teremos erro
+    val str: String? = null
+    println(str?.length) // (null) devido ser null não teremos o valor da propriedades, mas foi acessada
+
+    // alternativa para validar se null conforme acima:
+    if (str != null)
+        println(str.length) // essa forma é a mesma que acima usando o ?
+
+    // temos a alternativa de assumir que dará erro com !!
+    println(str!!.length) // dessa forma sabemos que dará erro e avisamos ao Kotlin para seguir, será tratado
 }
