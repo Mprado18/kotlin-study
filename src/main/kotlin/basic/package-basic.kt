8fb4021889b724f7e946911bf65f534bfe2948a7
Marcos-Prado <marcosprprado@yahoo.com.br>
2021-04-20 20:45:41 -0300

    about packages e imports packages

---
+++ src/main/kotlin/basic/package-basic.kt
@@ -0,0 +1,9 @@
+package basic
+
+fun hello() {
+    println("Olá função do arquivo no package basic")
+}
+
+fun sum() {
+    println("Somar 1 + 2 resulta em: ${1+2}")
+}
---
+++ src/main/kotlin/packages.kt
@@ -0,0 +1,23 @@
+// Os packages são pacotes, ou arquivos separados, exemplo classes, funções em outros arquivos
+// É realizado dessa forma para manter organizado e não deixar tudo em um local só, (SOLID)
+// Para utilizar outros packages, devemos importa-los.
+// Funcionado parecido com importações de componentes e módulos do React
+
+fun main() {
+    basic.hello() // função está em outro arquivo em outro package
+    hello() // função do root
+    basic.sum() // função está em outro arquivo em outro package
+}
+
+/*
+Se tentarmos usar uma função com mesmo nome de hello, teremos um erro de conflito, devido já conter uma função
+com este nome em outro pacote.
+Para isso podemos indicar colocando o nome do pacote a frente da função, exemplos acima.
+ */
+
+fun hello() {
+    println("Olá função do arquivo root")
+}
+
+// Algumas vezes são importados packages de funções Java para o Kotlin
+// Libs quando usamos alguma função são importadas da lib para nosso código
