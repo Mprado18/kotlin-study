c11fa497ce53799765582aa495463373f7f7a5a4
Marcos-Prado <marcosprprado@yahoo.com.br>
2021-04-20 23:11:44 -0300

    about strings functions

---
+++ src/main/kotlin/strings-methods.kt
@@ -0,0 +1,13 @@
+// Em Kotlin já temos algumas funções prontas para o tipo de dado String
+
+fun main() {
+    val str = "lorem ipsum"
+    println(str.capitalize()) //coloca primeira letra em maiúsculo
+    println(str.decapitalize()) //coloca primeira letra em maiúsculo
+    println(str.contains("test")) //valida se string contém x valor -> return boolean
+    println(str.startsWith("l")) //valida se string começa com x valor -> return boolean
+    println(str.startsWith("L")) //valida se string começa com x valor -> return boolean
+
+    //Existem diversas outras na documentação, cada uma para alguma finalidade.
+    //Também existem diversas funções prontas para arrays e listas
+}
