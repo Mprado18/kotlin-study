51982bdc96ee696bbc688a372fc5fe8f36976cd6
Marcos-Prado <marcosprprado@yahoo.com.br>
2021-04-21 17:18:52 -0300

    exercices 2

---
+++ src/main/kotlin/exercices/functions-exercices.kt
@@ -0,0 +1,24 @@
+package exercices
+
+/*
+1. Crie uma função que recebe uma String e retorne a quantidade de caracteres.
+*/
+fun str(text: String): Int {
+    val string = text
+    return text.length
+}
+
+/*
+2. Crie uma função que recebe uma String e mostre na tela a quantidade de caracteres.
+Resultado esperado: “A String -PARAMETRO- possui X caracteres.”
+Considere que PARAMETRO é o nome do parâmetro recebido pela função.
+*/
+fun str2(text: String) {
+    println("A string text: $text possui ${text.length} caracteres")
+}
+
+fun main() {
+    println(str("Hello"))
+
+    str2("TEXTO INSERIDO NO PARAMETRO")
+}
